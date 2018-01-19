124b3d3f697461149e1e41c450d730d8f6950ffa
Guo Yong Kang <guogin@outlook.com>
2018-01-19 15:25:36 +0800

    Runnable

--- src/main/java/com/edu/boot/FileStorageLoader.java
+++ src/main/java/com/edu/boot/FileStorageLoader.java
@@ -4,22 +4,25 @@ import com.edu.storage.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FileStorageLoader {
+@Order(1)
+public class FileStorageLoader implements CommandLineRunner {
     @Autowired
     private Environment environment;
 
-    @Bean
-    CommandLineRunner init(FileStorageService service) {
-        return (args) -> {
-            String ddlAuto = environment.getProperty("spring.jpa.hibernate.ddl-auto");
-            if (ddlAuto.equals("create-drop") || ddlAuto.equals("create")) {
-                service.deleteAll();
-            }
-            service.init();
-        };
+    @Autowired
+    FileStorageService service;
+
+    @Override
+    public void run(String... args) throws Exception {
+        String ddlAuto = environment.getProperty("spring.jpa.hibernate.ddl-auto");
+        if (ddlAuto.equals("create-drop") || ddlAuto.equals("create")) {
+            service.deleteAll();
+        }
+        service.init();
     }
 }
