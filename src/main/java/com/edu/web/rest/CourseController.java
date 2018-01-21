e06f42ea7c57e2928ca475c548ba06473941d1ce
Administrator <Administrator@USER-20161216KR>
2018-01-21 22:21:04 +0800

    comment CourseController

--- src/main/java/com/edu/web/rest/CourseController.java
+++ src/main/java/com/edu/web/rest/CourseController.java
@@ -26,37 +26,37 @@ import java.util.stream.Collectors;
 public class CourseController {
 	@Autowired
 	private CourseCategoryRepository courseCategoryRepository;
-	
+
 	@Autowired
 	private StudentRepository studentRepository;
-	
+
 	@Autowired
 	private CourseRepository courseRepository;
 
 	public static final String PATH = "/api/v1/Course";
-	
-	public static final String BOOK_PATH = PATH+"/book";
 
-	@PostMapping(path = PATH)
-	public ResponseEntity<List<CourseContainer>> getCoursesByCourseCategory(@RequestParam(value = "courseCategoryId") Long courseCategoryId,
-			HttpSession session) {
-		CourseCategory courseCategory = courseCategoryRepository.findOne(courseCategoryId);
-		String localDate = LocalDate.now().toString();
-		return new ResponseEntity<>(
-				courseCategory.getCourses().stream().filter(x -> x.getDate().compareTo(localDate) >= 0 ? true : false)
-						.sorted(Comparator.comparing(Course::getTimeFrom)).sorted(Comparator.comparing(Course::getDate))
-						.map(x -> new CourseContainer(x)).collect(Collectors.toCollection(ArrayList::new)),
-				HttpStatus.OK);
-	}
-	
-	@PostMapping(path = BOOK_PATH)
-	public ResponseEntity<ChildContainer> bookCourse(@RequestParam(value = "studentId") String studentId, @RequestParam(value = "courseId") Long courseId,
-			HttpSession session) {
-		Course course = courseRepository.findOne(courseId);
-		Student student = studentRepository.findOne(studentId);
-		student.addReservedCourse(course);
-		ChildContainer childContainer = new ChildContainer(studentRepository.save(student));
-		return new ResponseEntity<>(childContainer, HttpStatus.OK);
-	}
+	public static final String BOOK_PATH = PATH + "/book";
+
+//	@PostMapping(path = PATH)
+//	public ResponseEntity<List<CourseContainer>> getCoursesByCourseCategory(
+//			@RequestParam(value = "courseCategoryId") Long courseCategoryId, HttpSession session) {
+//		CourseCategory courseCategory = courseCategoryRepository.findOne(courseCategoryId);
+//		String localDate = LocalDate.now().toString();
+//		return new ResponseEntity<>(
+//				courseCategory.getCourses().stream().filter(x -> localDate.compareTo(x.getDate()) >= 0)
+//						.sorted(Comparator.comparing(Course::getTimeFrom)).sorted(Comparator.comparing(Course::getDate))
+//						.map(x -> new CourseContainer(x)).collect(Collectors.toCollection(ArrayList::new)),
+//				HttpStatus.OK);
+//	}
+//
+//	@PostMapping(path = BOOK_PATH)
+//	public ResponseEntity<ChildContainer> bookCourse(@RequestParam(value = "studentId") String studentId,
+//			@RequestParam(value = "courseId") Long courseId, HttpSession session) {
+//		Course course = courseRepository.findOne(courseId);
+//		Student student = studentRepository.findOne(studentId);
+//		student.addReservedCourse(course);
+//		ChildContainer childContainer = new ChildContainer(studentRepository.save(student));
+//		return new ResponseEntity<>(childContainer, HttpStatus.OK);
+//	}
 
 }
