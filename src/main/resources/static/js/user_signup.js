2c3ba20f4ddecab8b7b5e78071ab1152d4bd8f3e
Unknown <coolcathy1222@163.com>
2018-01-23 20:58:46 +0800

    signup html,js

--- src/main/resources/static/js/user_signup.js
+++ src/main/resources/static/js/user_signup.js
@@ -1,142 +1,135 @@
-var app = new Vue({
-    el: '#signup',
-    data: {
-        children: [
-            {
-                name: "",
-                birthday: ""
-            }
-        ],
-        mobilePhone: "",
-        verificationCode: "",
-        address: "",
-        removable: false,
-    },
-    methods: {
-        removeChild: function (index, child) {
-            app.children.splice(index, 1);
-            if (app.children.length == 1) {
-                app.removable = false;
-            }
-        },
-        saveBasic: function () {
-    		var mobilePhone = $("#mobilePhone")[0].value;
-    		var address = $("#address")[0].value;
-    		if(mobilePhone === ""){
-    			mobilePhone=$("#mobilePhone")[0].placeholder;
-    		}
-    		if(address === ""){
-    			address=$("#address")[0].placeholder;
-    		}
-    		$.post("/user/center?mobilePhone="+mobilePhone,"address="+address).done(function(data){
-    			alert(data);
-    		});
-        },
-        addChild: function () {
-            app.children.push({
-                name: "",
-                birthday: ""
+
+$(function(){
+    var wait = 60;
+    function timer(o) {
+        if (wait == 0) {
+            o.addClass("active");
+            o.text("获取验证码");
+            wait = 60;
+            return;
+        } else {
+            o.removeClass("active");
+            o.text(wait+"s后重发");
+            wait--;
+        }
+        setTimeout(function() {
+            timer(o);
+        },1000);
+    };
+    function validate(value, type){
+        var reg = "";
+        switch(type){
+            case "mobile":
+                reg = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/;
+                break;
+            default:
+                return false;
+        }
+
+        return reg.test(value);
+    }
+
+
+    $("#verifyCodeBtn").click(function(){
+        if(!$(this).hasClass("active")){
+            return false;
+        }
+        var mobileNumber = $("#mobileNumber").val();
+        var flag = validate(mobileNumber, "mobile");
+        if(!flag){
+            layer.open({
+                // type: 1,
+                // style: 'border:none; background-color:#ffcd0d; color:#000;',
+                content:"请您填写正确的手机号",
+                btn: ["确定"],
+                className: "popup" //这样你就可以在css里面控制该弹层的风格了
             });
-            if (app.children.length > 1) {
-                app.removable = true;
-            }
-        },
-        onClickGetVerificationCode: function () {
-        	var mobileNumber   = $('#mobileNumber').val();
-        	$.alert("您输入的手机号为:" + mobileNumber, "提醒");
-        	var oSMSObject = {
-        		mobile: mobileNumber,
-        		type: "R"
-        	};
-        	var wait = 5 * 60;
-        	var timer = function(o) {
-        		if (wait == 0) {
-        			o.attr('onclick',"onClickGetVerificationCode");
-	        		o.text("获取验证码");
-	        		wait = 5 * 60;
-	        		return;
-        		} else { 
-        			o.removeAttr('onclick');
-	        		o.text("重新发送(" + wait + ")");
-	        		wait--;
-        		}
-        		setTimeout(function() {
-        			timer(o);
-        		},1000);
-        	};
-        	
-        	$.ajax({
+        }else{
+            //调用发送验证码接口
+            var oSMSObject = {
+                mobile: mobileNumber,
+                type: "R"
+            };
+
+            $.ajax({
                 url: '/verify',
                 contentType: 'application/json',
                 type: 'POST',
                 data: JSON.stringify(oSMSObject),
             }).done(function (response) {
-            	$.alert("发送成功", "提醒");
-            	timer($('#verifyCodeBtn'));
-            	$('#wxVerifyCode').text(response);
-            });
-        },        
-
-        onAddStudents: function () {
-            var openCode   = $('#wxOpenCode').text();
-            var customerid = $('#customerid').text();
-            var students   = [];
-            var bHasError  = false;
-
-            for (var i = 0; i < app.children.length; ++i) {
-                var sChildName     = app.children[i].name.trim();
-                var sChildBirthday = app.children[i].birthday.trim();
-
-                if (sChildName === '' || sChildBirthday === '') {
-                    $.alert("请填写学员的姓名，生日信息", "提示!");
-                    bHasError = true;
-                    break;
-                } else {
-                    students.push({
-                        "childName": app.children[i].name,
-                        "birthday": app.children[i].birthday
-                    });
-                }
-            }
-
-            if (!bHasError || students.length === 0) {
-                $.ajax({
-                    url: '/api/v1/Customer/AddChild',
-                    contentType: 'application/json',
-                    type: 'POST',
-                    data: JSON.stringify(students),
-                }).done(function (response) {
-                    window.location.href = "/user/center";
+                layer.open({
+                    content: "发送成功",
+                    skin: "msg",
+                    time: 2
                 });
-            }
-        },
-        onSubmit: function () {
-        	var verifyCode = $('#wxVerifyCode').text();
-            var openCode = $('#wxOpenCode').text();
-            var verifyCodeId = $('#wxVerifyCode').text();
-            var customer = {
-                "openCode": openCode,
-                "name": app.children[0].name + "家长",
-                "mobilePhone": app.mobilePhone,
-                "address": app.address,
-                "verifyCodeId": verifyCode,
-                "children": []
-            };
-            for (var i = 0; i < app.children.length; ++i) {
-                customer.children.push({
-                    "childName": app.children[i].name,
-                    "birthday": app.children[i].birthday
-                });
-            }
-            
-            $.ajax({
-                url: '/api/v1/Customer/SignUp',
-                contentType: 'application/json',
-                type: 'POST',
-                data: JSON.stringify(customer),
-            }).done(function (response) {
-                window.location.href = "/user/center";
+                $(this).removeClass("active");
+                timer($('#verifyCodeBtn'));
+                $('#wxVerifyCode').val(response);
             });
-        },
-    }
+
+            // $.ajax({
+            //     type: "POST",
+            //     url: "/verify",
+            //     contentType: 'application/json',
+            //     data: JSON.stringify(oSMSObject),
+            //     success: function(data){
+            //         layer.open({
+            //             content: "发送成功",
+            //             skin: "msg",
+            //             time: 2
+            //         });
+            //         $(this).removeClass("active");
+            //         timer($('#verifyCodeBtn'));
+            //         $('#wxVerifyCode').val(data);
+            //     },
+            //     error: function(){
+            //         layer.open({
+            //             content: "错误，请稍后重试！",
+            //             skin: "msg",
+            //             time: 2
+            //         });
+            //     }
+            // });
+
+        }
+        return false;
+    });
+
+    $("#signup-form-btn").click(function(){
+        var mobileNumber = $("#mobileNumber").val();
+        var flag = validate(mobileNumber, "mobile");
+        if(!flag){
+            layer.open({
+                content:"请您填写正确的手机号",
+                btn: ["确定"],
+                className: "popup"
+            })
+        }
+        var captcha = $("#captcha").val();
+        // alert(mobileNumber+"==="+captcha);
+
+
+        var openCode = $("#wxOpenCode").val();
+        var verifyCodeId = $("#wxVerifyCode").val();
+        var verifyCode = $("#captcha").val();
+        var customer = {
+            "openCode": openCode,
+            "mobilePhone": mobileNumber,
+            "verifyCode": verifyCode,
+            "verifyCodeId": verifyCodeId,
+            "children": []
+        };
+
+        $.ajax({
+            url: '/api/v1/Customer/SignUp',
+            contentType: 'application/json',
+            type: 'POST',
+            data: JSON.stringify(customer),
+        }).done(function (response) {
+            window.location.href = "/user/center";
+        });
+
+        return false;
+    });
+
 });
