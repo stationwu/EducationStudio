d83c350658d32a3681bf180a26f497a7b1cbf8f0
Unknown <coolcathy1222@163.com>
2018-02-02 19:40:41 +0800

    js

--- src/main/webapp/js/course_list.js
+++ src/main/webapp/js/course_list.js
@@ -1,10 +1,32 @@
 $(function(){
     var student_id = GetQueryString("id");
 
+    // $.ajax({
+    //     type: "GET",
+    //     url: "/api/v1/Student?studentId="+student_id,
+    //     dataType: "json",
+    //     success: function(data){
+    //         var course = data;
+    //         var course_html = "";
+    //         for(var i=0; i<course.length; i++){
+    //             course_html += showCourse(course[i].id, (i+1), course[i].childName, course[i].course_name, course[i].time, course[i].address);
+    //         }
+    //         $("#subscribe-course").prepend(course_html);
+    //     },
+    //     error: function(){
+    //         msg_alert("alert", "错误，请稍后重试");
+    //     }
+    // });
+
+    var data = {
+        "studentId": student_id,
+    };
     $.ajax({
-        type: "GET",
-        url: "/api/v1/Student?studentId="+student_id,
+        type: "POST",
+        url: "/api/v1/Student",
         dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(data),
         success: function(data){
             var course = data;
             var course_html = "";
