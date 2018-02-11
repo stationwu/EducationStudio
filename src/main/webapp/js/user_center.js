8cffaab538b54b8f8d9a40bf089f40d3f67eac35
Unknown <coolcathy1222@163.com>
2018-02-11 17:55:56 +0800

    作品列表页面，学员中心页面

---
+++ src/main/webapp/js/user_center.js
@@ -0,0 +1,12 @@
+
+$(function(){
+    $("#courses").click(function(){
+        location.href = "/course/list";
+        return false;
+    });
+    $("#paintings").click(function(){
+        location.href = "";
+        return false;
+    });
+
+});
